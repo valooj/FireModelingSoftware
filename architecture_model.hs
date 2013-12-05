f23f708b26ffd25bed15af01419e5b7efd48de09
Sanzensekai <beckler.nathael@gmail.com>
2013-12-05 14:35:42 +0100

    Create architecture_model.hs

---
+++ architecture_model.hs
@@ -0,0 +1,71 @@
+import Text.ParserCombinators.Parsec
+
+main = do
+ f <- readFile "projet3A.txt"
+ let (Right m) = (parse ifc "" f)
+ putStrLn m
+
+type Comp  = String
+type Prop  = String
+type Value = String
+data Model = [(Comp,[(Prop,Value)])] | [Comp]
+  deriving Show
+
+ifc :: Parser Model
+ifc = many entry
+
+entry = do
+ string "#"
+ number_of_entry <- integer
+ string "="
+ name_of_entry <- idt
+ string "("
+ ps <- params
+ string ")"
+ string ";"
+ return (number_of_entry,name_of_entry,ps)
+ 
+integer = many1 digit
+
+params = do
+ { epsilon; }
+ <|> do {
+	p <- param;
+	c <- cont;
+	return (p,c)}
+
+
+param = do
+ string "#"
+ number_of_param <- integer
+ return number_of_param
+ <|> do
+	id <- idt
+	string "("
+	ps <- params
+	string ")"
+	return (id,ps)
+ <|> do
+	string "'"
+	txt <- text
+	string "'"
+	return txt
+
+cont = do
+	epsilon
+	<|> do
+	string ","
+	p <- param
+	c <- cont
+	return (p,c)
+	
+idt = do
+ identity <- many (letter <|> digit)
+ return identity
+ 
+text = do
+ txt <- many letter
+ return txt
+ 
+epsilon = do 
+ string ""

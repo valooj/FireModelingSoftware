62531c3d6a975cd85842cc9c71ced341af3bf61e
Sanzensekai <beckler.nathael@gmail.com>
2014-02-04 16:52:15 +0100

    Parser of IFC functional

--- simplify.hs
+++ simplify.hs
@@ -27,72 +27,19 @@ ifcentry = do
  i <- idtnumber
  string "= "
  name <- idtname
- string "("
+ opening
  prop <- ifcprop
+ closing
  eol
  return (i,name,prop)
 
-idtnumber = chiffre
-idtname =  alphabet
---idt = many ( digit <|> letter <|> char ',' <|> char '$'
--- <|> char ')' <|> char '\'' <|> char ' ' <|> char '('
--- <|> char '.' <|> char '#')
+idtnumber = many digit
+idtname = many (letter <|> digit)
 
 ifcprop = sepBy prop (char ',')
-prop = many (noneOf ",")
-eol = string ");"
-chiffre = many digit
-alphabet = many letter
-
---ip = many (letter <|> digit <|> char '.' <|> char ',')
---code permettant de distinguer les propriétés
-
--- ifcprp = do
- -- string "\'"
- -- sequence <- ip
- -- string "\'"
- -- return (sequence)
- -- <|>
- -- do
- -- string "#"
- -- numero <- chiffre -- on doit définir une nuovelle fonction
- -- return (numero)   -- car la précédente (idt) avale trop de caractères
- -- <|>
- -- do
- -- string "$"
- -- return ("$")
- -- <|>
- -- do
- -- string "."
- -- alpha <- alphabet
- -- string"."
- -- return (alpha)
- 
- 
-
---lib
--- type LibIdt = String
--- type LibName = String
--- type LibProp = [(String)]
--- type LibModel = [(LibIdt,LibName,LibProp)]
-
--- lib :: Parser LibModel
--- lib = many libentry
-
--- libentry = do
- -- string "&"
- -- i <- idt
- -- string "= "
- -- name <- idt
- -- string "("
- -- prop <- many libprp
- -- string ")"
--- string ";"
--- string "\n"
- -- return (i,name,prop)
-
--- libprp = do
- -- string "'"
- -- sequence <- idt
- -- string "'"
- -- return (sequence)
+prop = many (noneOf "=,();\n")
+eol = string ";\n"
+opening = try (string "((")
+ <|> string "("
+closing = try (string "))")
+ <|> string ")"
